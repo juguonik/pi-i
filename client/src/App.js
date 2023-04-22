d0b600f07131f121c5cdf75d8f8486bc4bcdf7a2
unknown <99849209+juguonik@users.noreply.github.com>
2023-04-22 13:42:53 -0300

    Adicionando elementos iniciais front e back

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,29 @@
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [material, setMaterial] = useState("");
+  const [quantidade, setQuantidade] = useState(0);
+
   return (
     <div className="App">
-      
+      <div className="information">
+        <label>Material:</label>
+        <input
+          type="text"
+          onChange={(event) => {
+            setMaterial(event.target.value);
+          }}
+        />
+        <label>Quantidade:</label>
+        <input
+          type="number"
+          onChange={(event) => {
+            setQuantidade(event.target.value);
+          }}
+        />
+        <button>Cadastrar</button>
+      </div>
     </div>
   );
 }
